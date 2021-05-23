9872e6e8e33b6266c5910bc8abfbb194287f2cd8
Sai Rohan P <psairohan21@gmail.com>
2021-05-23 19:51:57 +0530

    Created base.html,home.html and static css

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
-# Create your views here.
+posts = [
+    {
+        'author':'author1',
+        'title':'Title1',
+        'content':'Content1',
+        'date_posted':'May 23, 2021'
+    },
+    {
+        'author':'author2',
+        'title':'Title2',
+        'content':'Content2',
+        'date_posted':'May 23, 2021'
+    }
+]
+
+def home(request):
+    context = {
+        'posts':posts
+    }
+    return render(request,'blog/home.html',context)
