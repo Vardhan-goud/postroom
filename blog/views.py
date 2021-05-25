db00fc6b26b65238cec6944b169dbc702d6519b1
psksince2002 <saikoushik1978@gmail.com>
2021-05-25 17:25:45 +0530

    databases

--- blog/views.py
+++ blog/views.py
@@ -1,22 +1,11 @@
 from django.shortcuts import render
 
-posts = [
-    {
-        'author':'author1',
-        'title':'Title1',
-        'content':'Content1',
-        'date_posted':'May 23, 2021'
-    },
-    {
-        'author':'author2',
-        'title':'Title2',
-        'content':'Content2',
-        'date_posted':'May 23, 2021'
-    }
-]
+from .models import Post
+
+
 
 def home(request):
     context = {
-        'posts':posts
+        'posts':Post.objects.all()
     }
     return render(request,'blog/home.html',context)
