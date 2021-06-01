6610342d333ce3cf0486fab4233b594c04a70898
psksince2002 <saikoushik1978@gmail.com>
2021-06-01 22:35:23 +0530

    crud on post

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 # Create your models here.
 class Post(models.Model):
       title=models.CharField(max_length=100)
@@ -21,10 +22,15 @@ class Post(models.Model):
             ('travel','Travel'),
             ('pd','Personality Development'),
             ('med','Medicine'),
+            ('show','shows'),
+            ('movie','movies')
       ]
       tag = models.CharField(max_length=6, choices=TAG_CHOICES, default='misc')
       date_posted=models.DateTimeField(default=timezone.now)
       author=models.ForeignKey(User,on_delete=models.CASCADE)
       
-def __str__(self):
-      return self.title
+      def __str__(self):
+        return self.title
+      
+      def get_absolute_url(self):
+            return reverse('post-detail',kwargs={'pk':self.pk})
